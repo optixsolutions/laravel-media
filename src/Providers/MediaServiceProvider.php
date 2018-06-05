8ac085797d215ec58754929a390afaeaf353da86
Jack <jack@optixsolutions.co.uk>
2018-06-05 16:47:21 +0100

    MediaServiceProvider

--- src/Providers/MediaServiceProvider.php
+++ src/Providers/MediaServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Optix\Media\Providers;
+
 use Optix\Media\ConversionManager;
 use Illuminate\Support\ServiceProvider;
 
@@ -7,6 +9,12 @@ class MediaServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->publishes([
+            __DIR__ . '/../../database/migrations/create_media_table.php' => database_path(
+                'migrations/' . date('Y_m_d_His', time()) . '_create_media_table.php'
+            )
+        ], 'migrations');
+
         $this->app->singleton(ConversionManager::class);
     }
 }
