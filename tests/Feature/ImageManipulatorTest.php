733e3b36b7689a53924196d2ec440a3d593b2e11
andycoatz <andycoatz@gmail.com>
2019-02-15 17:23:26 +0000

    Test that already applied conversions are skipped

--- tests/Feature/ImageManipulatorTest.php
+++ tests/Feature/ImageManipulatorTest.php
@@ -2,6 +2,7 @@
 
 namespace Optix\Media\Tests;
 
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Intervention\Image\Image;
 use Intervention\Image\ImageManager;
 use Optix\Media\ConversionManager;
@@ -11,6 +12,7 @@ use Optix\Media\Models\Media;
 class ImageManipulatorTest extends TestCase
 {
     /**
+     * @test
      * @expectedException \Optix\Media\Exceptions\InvalidConversion
      */
     public function test_that_unknown_conversions_are_ignored()
@@ -22,6 +24,7 @@ class ImageManipulatorTest extends TestCase
         $manipulator->manipulate($media, ['random_conversion'], false);
     }
 
+    /** @test */
     public function test_that_known_conversions_are_applied()
     {
         $image = \Mockery::mock(Image::class);
@@ -49,5 +52,35 @@ class ImageManipulatorTest extends TestCase
 
         // Test that our converter was called, by checking if dummy object has been modified
         $this->assertEquals(1, $conversionTracker->conversionsPerformed);
+    }
+
+    /** @test */
+    public function test_that_existing_conversions_are_skipped()
+    {
+        // Create an object that our converter can modify
+        $conversionTracker = new \stdClass();
+        $conversionTracker->conversionsPerformed = 0;
+
+        // Register a converter
+        $resize = function (Image $image) use ($conversionTracker) {
+            $conversionTracker->conversionsPerformed++;
+
+            return $image;
+        };
+        $conversionManager = new ConversionManager();
+        $conversionManager->register('resize', $resize);
+
+        // Mock that our converted image already exists on the filesystem
+        $filesystem = \Mockery::mock(Filesystem::class);
+        $filesystem->shouldReceive('exists')->andReturn(true);
+
+        /** @var Media $media */
+        $media = \Mockery::mock(Media::class)->makePartial();
+        $imageManager = \Mockery::mock(ImageManager::class);
+        $manipulator = new ImageManipulator($conversionManager, $imageManager);
+        $manipulator->manipulate($media, ['resize'], true);
+
+        // Test that our converter was *not* called
+        $this->assertEquals(0, $conversionTracker->conversionsPerformed);
     }
 }
