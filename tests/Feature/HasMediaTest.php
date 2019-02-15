2cd6c8f367cb2d8bfac254d2378da5472b686aed
Jack Robertson <jcrobertson97@gmail.com>
2019-02-15 11:51:50 +0000

    Setup database

--- tests/Feature/HasMediaTest.php
+++ tests/Feature/HasMediaTest.php
@@ -1,22 +1,29 @@
 <?php
 
-namespace Optix\Media\Tests;
+namespace Optix\Media\Tests\Feature;
+
+use Optix\Media\Tests\TestCase;
+use Optix\Media\Tests\TestModel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class HasMediaTest extends TestCase
 {
+    use RefreshDatabase;
+
     protected $testModel;
 
     protected function setUp()
     {
         parent::setUp();
 
-        // $this->testModel = TestModel::create();
+        $this->testModel = TestModel::create();
     }
 
     /** @test */
     public function it_can_determine_if_a_model_has_media_in_a_specified_group()
     {
         $this->assertTrue($this->testModel->hasMedia()); // default
+        $this->assertTrue($this->testModel->HasMedia(''));
         $this->assertFalse($this->testModel->hasMedia('an-empty-group'));
     }
 }
