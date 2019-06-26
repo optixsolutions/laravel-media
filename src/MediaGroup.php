750415e17078340f0546da8189cdc82fb79910bd
Jack Robertson <jack@optixsolutions.co.uk>
2019-06-26 12:14:27 +0100

    Doc block the media group

--- src/MediaGroup.php
+++ src/MediaGroup.php
@@ -4,8 +4,17 @@ namespace Optix\Media;
 
 class MediaGroup
 {
+    /**
+     * @var array
+     */
     protected $conversions = [];
 
+    /**
+     * Register the conversions to be performed when media is attached.
+     *
+     * @param  mixed  ...$conversions
+     * @return $this
+     */
     public function performConversions(...$conversions)
     {
         $this->conversions = $conversions;
@@ -13,11 +22,21 @@ class MediaGroup
         return $this;
     }
 
+    /**
+     * Determine if the group has any registered conversions.
+     *
+     * @return bool
+     */
     public function hasConversions()
     {
         return ! empty($this->conversions);
     }
 
+    /**
+     * Get all the registered conversions.
+     *
+     * @return array
+     */
     public function getConversions()
     {
         return $this->conversions;
