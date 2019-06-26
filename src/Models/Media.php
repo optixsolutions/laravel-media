3963672caab67055a8e5b85cea1ff58857aad53f
Jack Robertson <jack@optixsolutions.co.uk>
2019-06-26 12:55:00 +0100

    Doc block media model

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -4,38 +4,78 @@ namespace Optix\Media\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Contracts\Filesystem\Filesystem;
 
 class Media extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'name', 'file_name', 'disk', 'mime_type', 'size'
     ];
 
+    /**
+     * Get the file extension.
+     *
+     * @return string
+     */
     public function getExtensionAttribute()
     {
         return pathinfo($this->file_name, PATHINFO_EXTENSION);
     }
 
+    /**
+     * Get the file type.
+     *
+     * @return string|null
+     */
     public function getTypeAttribute()
     {
         return str_before($this->mime_type, '/') ?? null;
     }
 
+    /**
+     * Determine if the file is of the specified type.
+     *
+     * @param  string  $type
+     * @return bool
+     */
     public function isOfType(string $type)
     {
         return $this->type === $type;
     }
 
+    /**
+     * Get the url to the file.
+     *
+     * @param  string  $conversion
+     * @return mixed
+     */
     public function getUrl(string $conversion = '')
     {
         return $this->filesystem()->url($this->getPath($conversion));
     }
 
+    /**
+     * Get the full path to the file.
+     *
+     * @param  string  $conversion
+     * @return mixed
+     */
     public function getFullPath(string $conversion = '')
     {
         return $this->filesystem()->path($this->getPath($conversion));
     }
 
+    /**
+     * Get the path to the file on disk.
+     *
+     * @param  string  $conversion
+     * @return string
+     */
     public function getPath(string $conversion = '')
     {
         $directory = $this->getDirectory();
@@ -47,11 +87,21 @@ class Media extends Model
         return $directory . '/' . $this->file_name;
     }
 
+    /**
+     * Get the directory for files on disk.
+     *
+     * @return mixed
+     */
     public function getDirectory()
     {
         return $this->getKey();
     }
-    
+
+    /**
+     * Get the filesystem where the associated file is stored.
+     *
+     * @return Filesystem
+     */
     public function filesystem()
     {
         return Storage::disk($this->disk);
