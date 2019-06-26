93192c1e4c1319be468182a202c14d709f99567e
Jack Robertson <jack@optixsolutions.co.uk>
2019-06-26 11:30:26 +0100

    Doc block the conversion registry

--- src/ConversionRegistry.php
+++ src/ConversionRegistry.php
@@ -6,18 +6,39 @@ use Optix\Media\Exceptions\InvalidConversion;
 
 class ConversionRegistry
 {
+    /**
+     * @var array
+     */
     protected $conversions = [];
 
+    /**
+     * Get all the registered conversions.
+     *
+     * @return array
+     */
     public function all()
     {
         return $this->conversions;
     }
 
+    /**
+     * Register a new conversion.
+     *
+     * @param  string  $name
+     * @param  callable  $conversion
+     * @return void
+     */
     public function register(string $name, callable $conversion)
     {
         $this->conversions[$name] = $conversion;
     }
 
+    /**
+     * Get a registered conversion with the specified name.
+     *
+     * @param  string  $name
+     * @return mixed
+     */
     public function get(string $name)
     {
         if (! $this->exists($name)) {
@@ -27,6 +48,12 @@ class ConversionRegistry
         return $this->conversions[$name];
     }
 
+    /**
+     * Determine if a conversion with the specified name exists.
+     *
+     * @param  string  $name
+     * @return bool
+     */
     public function exists(string $name)
     {
         return isset($this->conversions[$name]);
