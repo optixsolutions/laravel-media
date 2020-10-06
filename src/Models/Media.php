4ddeafec15991b2158a693d259c38f75601539d1
Jack Robertson <jack@optixsolutions.co.uk>
2020-10-06 14:45:19 +0100

    Be explicit with the media table name to avoid pluralisation issues

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -10,6 +10,13 @@ use Illuminate\Support\Str;
 class Media extends Model
 {
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'media';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
